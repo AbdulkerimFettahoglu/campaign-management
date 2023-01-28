e1a59a11d779a6ae7c7a9362293e5e9c842a999b
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-01-28 21:03:44 +0300

    count query added to find which status how many record has.

--- src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignRepository.java
+++ src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignRepository.java
@@ -3,6 +3,7 @@ package com.kerimfettahoglu.campaignmanagement.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.kerimfettahoglu.campaignmanagement.entity.Campaign;
 import com.kerimfettahoglu.campaignmanagement.entity.Category;
@@ -10,4 +11,7 @@ import com.kerimfettahoglu.campaignmanagement.entity.Category;
 public interface CampaignRepository extends JpaRepository<Campaign, Integer>{
 	
 	public List<Campaign> findByCategoryAndTitleAndDetails(Category category, String title, String description);
+	
+	@Query("select a.status.id, count(a) from Campaign a group by a.status.id")
+	public List<Object[]> countByStatus();
 }
