c3d244f5c9da1e7f8d279761573eea3f091f6a93
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-01-28 22:25:52 +0300

    new query was added to log repository to find history of a campaign.

--- src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignLogRepository.java
+++ src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignLogRepository.java
@@ -1,9 +1,12 @@
 package com.kerimfettahoglu.campaignmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kerimfettahoglu.campaignmanagement.entity.CampaignLog;
 
 public interface CampaignLogRepository extends JpaRepository<CampaignLog, Integer> {
 
+	public List<CampaignLog> findByIdOrderByLogDate(Integer campaignId);
 }
