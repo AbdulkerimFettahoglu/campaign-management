cd4e6d84b0c713c5f792fdd35d3be116942b84c5
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-01-28 20:30:29 +0300

    activate-deactivate functions added.

--- src/main/java/com/kerimfettahoglu/campaignmanagement/service/CampaignService.java
+++ src/main/java/com/kerimfettahoglu/campaignmanagement/service/CampaignService.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.kerimfettahoglu.campaignmanagement.dto.CreateCampaignDto;
 import com.kerimfettahoglu.campaignmanagement.entity.Campaign;
+import com.kerimfettahoglu.campaignmanagement.entity.Category;
 import com.kerimfettahoglu.campaignmanagement.enumaration.CategoryEnum;
 import com.kerimfettahoglu.campaignmanagement.enumaration.StatusEnum;
 import com.kerimfettahoglu.campaignmanagement.repository.CampaignRepository;
+import com.kerimfettahoglu.campaignmanagement.repository.CategoryRepository;
 import com.kerimfettahoglu.campaignmanagement.repository.StatusRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,11 +20,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class CampaignService {
 	
-	private final CampaignRepository campaignRepostiory;
+	private final CampaignRepository campaignRepository;
 	private final StatusRepository statusRepository;
+	private final CategoryRepository categoryRepostiory;
 
 	public Campaign get(Integer campaignId) {
-		Optional<Campaign> campaign = campaignRepostiory.findById(campaignId);
+		Optional<Campaign> campaign = campaignRepository.findById(campaignId);
 		if (campaign.isPresent())
 			return campaign.get();
 		else
@@ -30,13 +33,20 @@ public class CampaignService {
 	}
 
 	public Campaign create(CreateCampaignDto createCampaignDto) {
-		CategoryEnum cat = CategoryEnum.getCategory(createCampaignDto.getCategory());
 		Campaign campaign = CreateCampaignDto.mapDtoToObj(createCampaignDto);
+		specifyCategory(campaign, createCampaignDto);
 		specifyStatus(campaign);
-		campaignRepostiory.save(campaign);
+		campaignRepository.save(campaign);
 		return campaign;
 	}
 	
+	private void specifyCategory(Campaign campaign, CreateCampaignDto dto) {
+		Optional<Category> cat = categoryRepostiory.findById(dto.getCategory());
+		if (cat.isEmpty())
+			throw new RuntimeException("Böyle bir kategori bulunamadı.");
+		campaign.setCategory(cat.get());
+	}
+
 	private void specifyStatus(Campaign campaign) {
 		if (isMukerrer(campaign)) {
 			campaign.setStatus(statusRepository.findById(StatusEnum.MUKERRER.getId()).get());
@@ -48,9 +58,31 @@ public class CampaignService {
 	}
 	
 	public Boolean isMukerrer(Campaign campaign) {
-		List<Campaign> result = campaignRepostiory.findByCategoryAndTitleAndDetails(campaign.getCategory(), campaign.getTitle(), campaign.getDetails());
+		List<Campaign> result = campaignRepository.findByCategoryAndTitleAndDetails(campaign.getCategory(), campaign.getTitle(), campaign.getDetails());
 		if (result.isEmpty())
 			return false;
 		return true;
+	}
+	
+	public Boolean activateCampaign(Integer campaignId) {
+		Optional<Campaign> optCamp = campaignRepository.findById(campaignId);
+		if (optCamp.isPresent() && (StatusEnum.ONAY_BEKLIYOR.getId().equals(optCamp.get().getStatus().getId()))) {
+			optCamp.get().setStatus(statusRepository.findById(StatusEnum.AKTIF.getId()).get());
+			campaignRepository.save(optCamp.get());
+			return true;
+		} else {
+			throw new RuntimeException("Kampanya bulunaadı veya durumu " + StatusEnum.ONAY_BEKLIYOR.getDescription() + " değil.");
+		}
+	}
+
+	public Boolean deactivateCampaign(Integer campaignId) {
+		Optional<Campaign> optCamp = campaignRepository.findById(campaignId);
+		if (optCamp.isPresent() && (StatusEnum.ONAY_BEKLIYOR.getId().equals(optCamp.get().getStatus().getId()) || StatusEnum.AKTIF.getId().equals(optCamp.get().getStatus().getId()))) {
+			optCamp.get().setStatus(statusRepository.findById(StatusEnum.DEAKTIF.getId()).get());
+			campaignRepository.save(optCamp.get());
+			return true;
+		} else {
+			throw new RuntimeException("Kampanya bulunaadı veya durumu " + StatusEnum.ONAY_BEKLIYOR.getDescription() + "," + StatusEnum.AKTIF.getDescription() + " değil.");
+		}
 	}
 }
