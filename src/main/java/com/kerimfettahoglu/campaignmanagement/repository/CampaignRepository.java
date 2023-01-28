58a79e408aa84ec481d0e6f4c1acfbc0b8b65ef4
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-01-28 19:10:21 +0300

    new query was added to check uniqueness.

--- src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignRepository.java
+++ src/main/java/com/kerimfettahoglu/campaignmanagement/repository/CampaignRepository.java
@@ -1,9 +1,13 @@
 package com.kerimfettahoglu.campaignmanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kerimfettahoglu.campaignmanagement.entity.Campaign;
+import com.kerimfettahoglu.campaignmanagement.entity.Category;
 
 public interface CampaignRepository extends JpaRepository<Campaign, Integer>{
-
+	
+	public List<Campaign> findByCategoryAndTitleAndDetails(Category category, String title, String description);
 }
