25048fe13c50b67f9a031c62eda9134def567dc7
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-01-29 18:00:51 +0300

    unnecessary imports removed

--- src/main/java/com/kerimfettahoglu/campaignmanagement/service/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/kerimfettahoglu/campaignmanagement/service/exception/ApplicationExceptionHandler.java
@@ -1,20 +1,9 @@
 package com.kerimfettahoglu.campaignmanagement.service.exception;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.persistence.RollbackException;
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.TransactionSystemException;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
